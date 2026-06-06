f352c0ae3763d4736ed837c09573d19c21722b09
leo385 <leonsoshi@gmail.com>
2026-06-07 00:09:52 +0200

    Make /api/login for LoginForm

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const db = require('./db');
 
 const app = express();
 const PORT = 4000;
@@ -9,6 +10,25 @@ app.use(express.json());
 
 app.get('/api/status', (req, res) => {
     res.json({ message: 'Backend Express.js działa i jest gotowy na przyjęcie rezerwacji!'});
+});
+
+app.post('/api/login', (req, res) => {
+    const { email, password } = req.body;
+
+    /*  Find user email in database */
+    db.get(`SELECT * FROM users WHERE email = ?`, [email], (err, user) => {
+        if(err) {
+            return res.status(500).json({ error: "Błąd serwera bazy danych." });
+        }
+
+        if(!user || user.password !== password) {
+            return res.status(401).json({ error: "Błędna nazwa uzytkownika lub hasło."});
+        }
+
+        if(user.password === password) {
+            return res.json({ message: "Zalogowano pomyślnie!", email: user.email });
+        }
+    });
 });
 
 app.listen(PORT, () => {
